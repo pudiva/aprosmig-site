5d54e48254f3231724f40ccf3db43120f2727a82
pudiva <rogi@skylittlesystem.org>
2020-11-11 16:34:56 +0000

    Usa gem "yt" ao invés de `youtube-dl`, que está bugado com playlists

--- pull-vozes-da-guaicurus-from-youtube.rb
+++ pull-vozes-da-guaicurus-from-youtube.rb
@@ -1,18 +1,22 @@
-require "json"
+# you'll need an youtube API KEY:
+# export YT_API_KEY="badbadbadbadbadbadbadbadbad"
+
+require "active_support"
+require "active_support/core_ext"
+require "byebug"
 require "yaml"
-require 'active_support'
-require 'active_support/core_ext'
+require "yt"
 
-jason = JSON.parse(`youtube-dl -J PLOVo8bItdVh8_IQfEZVGdrBNhWQNI8JiZ`)
-
-jason["entries"].map do |entry|
-  image = entry["thumbnails"].last
-  upload_date = Date.iso8601(entry["upload_date"])
+list = Yt::Playlist.new id: "PLOVo8bItdVh8_IQfEZVGdrBNhWQNI8JiZ"
+list.playlist_items.map do |entry|
+  next unless entry.public?
+  image = entry.snippet.thumbnails["maxres"]
+  upload_time = entry.published_at.in_time_zone("America/Sao_Paulo")
 
   out = {
     "layout" => "vozes-da-guaicurus-post",
-    "title" => entry["title"],
-    "date" => upload_date.iso8601,
+    "title" => entry.title,
+    "date" => upload_time.iso8601,
     "categories" => "vozes-da-guaicurus",
     "image" => {
       "path" => image["url"],
@@ -20,19 +24,19 @@ jason["entries"].map do |entry|
       "height" => image["height"],
     },
     "video" => {
-      "path" => "https://www.youtube.com/embed/#{entry["id"]}?rel=0",
+      "path" => "https://www.youtube.com/embed/#{entry.video_id}?rel=0",
       "type" => "text/html",
       "width" => image["width"],
-      "height" => image["height"]
+      "height" => image["height"],
     },
     "seo" => {
       "type" => "CreativeWork",
     },
   }
 
-  File.open("_posts/#{upload_date.iso8601}-#{entry["title"].parameterize}.md", "w") do |fp|
+  File.open("_posts/#{upload_time.to_date.iso8601}-#{entry.title.parameterize}.md", "w") do |fp|
     fp.puts YAML.dump(out)
     fp.puts "---"
-    fp.puts entry["description"]
+    fp.puts entry.description
   end
 end
